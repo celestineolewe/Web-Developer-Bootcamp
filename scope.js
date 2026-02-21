1596b8761ed1eb66c930cccb1f043cd17e6d31b3
Celestine Imelda <celestineolewe@gmail.com>
2026-02-21 17:09:00 +0300

    Section 21:Levelling Up Our Functions

---
+++ scope.js
@@ -0,0 +1,37 @@
+
+// let totalEggs = 0;
+// function collectEggs() {
+//     totalEggs = 6;
+    
+// }
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs());
+
+// let bird = 'Scarlet Macaw';
+// function birdWatch() {
+//     let bird = 'Great Blue Heron';
+//     console.log(bird);
+// }
+
+// let animal = "Giant Pacific Octopus";
+// function observe() {
+//     let animal = "Pajama Squid";
+//     console.log(animal);
+// }
+// observe();
+
+// const creature = 'Common Sea Dragon';
+// function scubaDive() {
+//     const creature = 'Spanish Dancer';
+//     console.log(creature);
+// }
+// scubaDive();
+
+let deadlyAnimal = 'Blue-Ringed Octopus';
+function handleAnimal() {
+    let deadlyAnimal = 'Scorpion Fish';
+    console.log(deadlyAnimal);
+}   
+handleAnimal();
+console.log(deadlyAnimal);
---
+++ this.js
@@ -0,0 +1,16 @@
+// const cat = {
+//     name: 'Missy',
+//     age: 3,
+//     color: 'orange',
+//     meow() {
+//         console.log(this.name);
+//     }
+// }
+// const meow2 = cat.meow;
+// meow2(); // undefined
+
+try {
+    hello.toUpperCase();
+} catch (error) {
+    console.log("Error:", error.message);   
+}
